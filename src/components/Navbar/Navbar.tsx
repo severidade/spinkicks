7f69ffccd33cfd4cc410b810c7e0123254810c81
severo <oliveira.severo@gmail.com>
2024-11-13 19:37:25 -0300

    style: :lipstick: estilos iniciais para nav demo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,16 @@
-
+import styles from './navbar.module.css';
 import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
-    <nav>
-      <NavLink to="/" className="link">Demo 01</NavLink>
-      <NavLink to="/demo02" className="link">Demo 02</NavLink>
+    <nav className={styles.container_nav_link}>
+      <NavLink to="/" className={({ isActive }) => `${styles.nav_link} ${isActive && styles.active} link`} >
+        Demo 01
+      </NavLink>
+      <NavLink to="/demo02" className={({ isActive }) => `${styles.nav_link} ${isActive && styles.active} link`}>
+          Demo 02
+      </NavLink>
     </nav>
   );
 }
 
 export default Navbar;
-
-// className={`${styles.social_linkedin} link`}
