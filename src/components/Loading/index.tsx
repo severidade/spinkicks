787489d367308ec207d63920f8225ca62c1326b8
severo <oliveira.severo@gmail.com>
2024-11-26 11:44:03 -0300

    test: :test_tube: Cria teste para o componente Loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,10 @@ import styles from "./loading.module.css"
 export default function Loading() {
   return (
     <div className={styles.loading}>
-      <div className={styles.spinner} />
+      <div
+        className={styles.spinner} 
+        data-testid="spinner" 
+      />
       <div className={styles.loading_text}>Carregando</div>
     </div>
   );
